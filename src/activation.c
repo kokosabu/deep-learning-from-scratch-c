7356a4bd89b63128b8d503a2ab59c38cbf4e617d
MIURA Yasuyuki <kokosabu@gmail.com>
2018-10-16 23:14:36 +0900

    update softmax function

--- src/activation.c
+++ src/activation.c
@@ -1,5 +1,22 @@
 #include <activation.h>
 #include <math.h>
+#include <float.h>
+
+static double max(double a[], size_t s)
+{
+    int i;
+    double m;
+
+    m = DBL_MIN;
+
+    for(i = 0; i < s; i++) {
+        if(a[i] > m) {
+            m = a[i];
+        }
+    }
+
+    return m;
+}
 
 double step_function(double x)
 {
@@ -33,13 +50,16 @@ void softmax(double y[], double a[], size_t s)
 {
     int i;
     double sum_exp;
+    double c;
+
+    c = max(a, s);
 
     sum_exp = 0.0;
     for(i = 0; i < s; i++) {
-        sum_exp += exp(a[i]);
+        sum_exp += exp(a[i] - c);
     }
 
     for(i = 0; i < s; i++) {
-        y[i] = exp(a[i]) / sum_exp;
+        y[i] = exp(a[i] - c) / sum_exp;
     }
 }
