03508d80aa3dae73eb9d75e11cfda05e164b74e8
MIURA Yasuyuki <kokosabu@gmail.com>
2018-10-28 01:44:28 +0900

    add mean squared error test case

--- test/test-loss.c
+++ test/test-loss.c
@@ -3,9 +3,12 @@
 
 void test_mean_squared_error(void)
 {
-    double y[] = {0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0};
+    double y1[] = {0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0};
+    double y2[] = {0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.5, 0.0};
     double t[] = {0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
 
-    printf("%f\n", mean_squared_error(y, t, sizeof(y)/sizeof(y[0])));
-    cut_assert(mean_squared_error(y, t, sizeof(y)/sizeof(y[0])) == 0.0975);
+    cut_assert(mean_squared_error(y1, t, sizeof(y1)/sizeof(y1[0])) >= 0.0975);
+    cut_assert(mean_squared_error(y1, t, sizeof(y1)/sizeof(y1[0])) <= 0.0976);
+    cut_assert(mean_squared_error(y2, t, sizeof(y2)/sizeof(y2[0])) >= 0.72249);
+    cut_assert(mean_squared_error(y2, t, sizeof(y2)/sizeof(y2[0])) <= 0.72251);
 }
