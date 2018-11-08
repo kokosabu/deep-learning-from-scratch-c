3f87e34b5d08b93cc92342e2853636adb66dd828
MIURA Yasuyuki <kokosabu@gmail.com>
2018-11-08 23:29:38 +0900

    add simpleNet

--- test/test-simple_net.c
+++ test/test-simple_net.c
@@ -1,32 +1,45 @@
 #include <cutter.h>
 #include <simple_net.h>
+#include <grad.h>
+
+double x[] = {0.6, 0.9};
+double t[] = {0, 0, 1};
+
+static double f(double *w)
+{
+    return loss(x, t);
+}
 
 void test_simple_net(void)
 {
     double **W;
-    int i;
-    int j;
-    double x[] = {0.6, 0.9};
+    double *grad;
     double *p;
-    double t[] = {0, 0, 1};
 
     simpleNet();
     W = get_W();
 
-    //W[0][0] = 
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 3; j++) {
-            printf("%f ", W[i][j]);
-        }
-        printf("\n");
-    }
+    W[0][0] = 0.47355232;
+    W[0][1] = 0.9977393;
+    W[0][2] = 0.84668094;
+    W[1][0] = 0.85557411;
+    W[1][1] = 0.03563661;
+    W[1][2] = 0.69422093;
 
     predict(&p, x);
+    cut_assert(p[0] >= 1.0541480);
+    cut_assert(p[0] <= 1.0541481);
+    cut_assert(p[1] >= 0.6307165);
+    cut_assert(p[1] <= 0.6307166);
+    cut_assert(p[2] >= 1.132807);
+    cut_assert(p[2] <= 1.132808);
 
-    for(i = 0; i < 3; i++) {
-        printf("%f ", p[i]);
-    }
-    printf("\n");
+    cut_assert(loss(x, t) >= 0.92806);
+    cut_assert(loss(x, t) <= 0.92807);
 
-    printf("%f\n", loss(x, t));
+    numerical_gradient(&grad, f, *W, 3);
+    printf("%f\n", grad[0]);
+    printf("%f\n", grad[1]);
+    printf("%f\n", grad[2]);
 }
+
