739722c00b7a9902a3e944ba2619ddf1396bf61a
MIURA Yasuyuki <kokosabu@gmail.com>
2018-11-18 22:50:07 +0900

    [WIP] add two layer net

--- test/test-two_layer_net.c
+++ test/test-two_layer_net.c
@@ -16,30 +16,32 @@ void test_simple_net(void)
     double *grad;
     double *p;
 
-    simpleNet();
-    W = get_W();
+    TwoLayerNet();
 
-    W[0][0] = 0.47355232;
-    W[0][1] = 0.9977393;
-    W[0][2] = 0.84668094;
-    W[1][0] = 0.85557411;
-    W[1][1] = 0.03563661;
-    W[1][2] = 0.69422093;
+    //simpleNet();
+    //W = get_W();
 
-    predict(&p, x);
-    cut_assert(p[0] >= 1.0541480);
-    cut_assert(p[0] <= 1.0541481);
-    cut_assert(p[1] >= 0.6307165);
-    cut_assert(p[1] <= 0.6307166);
-    cut_assert(p[2] >= 1.132807);
-    cut_assert(p[2] <= 1.132808);
+    //W[0][0] = 0.47355232;
+    //W[0][1] = 0.9977393;
+    //W[0][2] = 0.84668094;
+    //W[1][0] = 0.85557411;
+    //W[1][1] = 0.03563661;
+    //W[1][2] = 0.69422093;
 
-    cut_assert(loss(x, t) >= 0.92806);
-    cut_assert(loss(x, t) <= 0.92807);
+    //predict(&p, x);
+    //cut_assert(p[0] >= 1.0541480);
+    //cut_assert(p[0] <= 1.0541481);
+    //cut_assert(p[1] >= 0.6307165);
+    //cut_assert(p[1] <= 0.6307166);
+    //cut_assert(p[2] >= 1.132807);
+    //cut_assert(p[2] <= 1.132808);
 
-    numerical_gradient(&grad, f, *W, 3);
-    printf("%f\n", grad[0]);
-    printf("%f\n", grad[1]);
-    printf("%f\n", grad[2]);
+    //cut_assert(loss(x, t) >= 0.92806);
+    //cut_assert(loss(x, t) <= 0.92807);
+
+    //numerical_gradient(&grad, f, *W, 3);
+    //printf("%f\n", grad[0]);
+    //printf("%f\n", grad[1]);
+    //printf("%f\n", grad[2]);
 }
 
