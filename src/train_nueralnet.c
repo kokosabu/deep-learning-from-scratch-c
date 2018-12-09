d4a2b24aafe8152504d87426af7d625e2d5afb0a
MIURA Yasuyuki <kokosabu@gmail.com>
2018-12-09 17:38:43 +0900

    [WIP] add train_nueralnet

--- src/train_nueralnet.c
+++ src/train_nueralnet.c
@@ -46,6 +46,15 @@ int main()
     double **grads;
     double *train_loss_list;
 
+    double **W1;
+    double **W2;
+    double *b1;
+    double *b2;
+    double **grad_W1;
+    double **grad_W2;
+    double *grad_b1;
+    double *grad_b2;
+
     read_images(&x_train_, &x_train,  "train-images-idx3-ubyte", 0);
     read_labels(&t_train, "train-labels-idx1-ubyte");
     read_images(&x_test_,  &x_test, "t10k-images-idx3-ubyte", 0);
@@ -53,7 +62,7 @@ int main()
 
     train_size = 60000;
     //iters_num = 10000;
-    iters_num = 2;
+    iters_num = 1;
     batch_size = 100;
     learning_rate = 0.01;
 
@@ -80,6 +89,25 @@ int main()
 
         /* 勾配の計算 */
         TwoLayerNet_numerical_gradient(&grads, x_batch, t_batch);
+
+        W1 = get_W1();
+        b1 = get_b1();
+        W2 = get_W2();
+        b2 = get_b2();
+
+        grad_W1 = get_grad_W1();
+        grad_b1 = get_grad_b1();
+        grad_W2 = get_grad_W2();
+        grad_b2 = get_grad_b2();
+
+        printf("W1[0][0] = %f\n", W1[0][0]);
+        printf("b1[0]    = %f\n", b1[0]);
+        printf("W2[0][0] = %f\n", W2[0][0]);
+        printf("b2[0]    = %f\n", b2[0]);
+        printf("grad_W1[0][0] = %f\n", grad_W1[0][0]);
+        printf("grad_b1[0]    = %f\n", grad_b1[0]);
+        printf("grad_W2[0][0] = %f\n", grad_W2[0][0]);
+        printf("grad_b2[0]    = %f\n", grad_b2[0]);
 
         /* パラメータの更新 */
         W1_update(learning_rate);
